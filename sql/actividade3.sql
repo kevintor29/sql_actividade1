685e3c1701d6ca34aee6ea8d6414773b2577acf1
kevintor29 <112710345+kevintor29@users.noreply.github.com>
2022-10-03 12:59:51 -0300

    Add files via upload

---
+++ sql/actividade3.sql
@@ -0,0 +1,24 @@
+/*create database escola;
+use escola;
+create table estundantes(
+id int(20) auto_increment,
+nome varchar(30) not null,
+sobrenome varchar(40) not null,
+idade bigint(3),
+notas decimal(2,1),
+primary key (id)
+);*/
+
+/*use escola;
+insert into estundantes(nome,sobrenome,idade,notas) values("Crstian","Torres",14,5.6);
+insert into estundantes(nome,sobrenome,idade,notas) values("Josue","Ramirez",12,4.6);
+insert into estundantes(nome,sobrenome,idade,notas) values("Alexis","Jorman",16,9.5);
+insert into estundantes(nome,sobrenome,idade,notas) values("Jerkin","Averdaño",15,0.3);
+insert into estundantes(nome,sobrenome,idade,notas) values("Jeremy","cumana",15,8.0);
+insert into estundantes(nome,sobrenome,idade,notas) values("kaio","lorca",15,9.0);
+insert into estundantes(nome,sobrenome,idade,notas) values("tutancamoto","barlod",15,7.8);
+insert into estundantes(nome,sobrenome,idade,notas) values("tutu","momon",15,8.6);
+
+select * from estundantes where notas>7.0;
+select * from estundantes where notas<7.0;*/
+select * from estundantes;
